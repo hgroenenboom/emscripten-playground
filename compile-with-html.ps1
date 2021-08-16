cc57642305992d75c5317eee3f48d74c24e21c83
Harold Groenenboom <harold_groe@hotmail.com>
2021-08-16 23:01:14 +0200

    Added emscripten as submodule

--- compile-with-html.ps1
+++ compile-with-html.ps1
@@ -1,1 +1,3 @@
+. "$PSScriptRoot/submodules/emsdk/emsdk_env.ps1"
+
 emcc $PSScriptRoot/source/main.c -o $PSScriptRoot/build/index.html
--- initialize.ps1
+++ initialize.ps1
@@ -1,4 +1,17 @@
 git submodule update --init --recursive
 
+cd "$PSScriptRoot/submodules/emsdk" 
+
+# Fetch the latest version of the emsdk (not needed the first time you clone)
+git pull
+
+# Download and install the latest SDK tools.
+./emsdk install latest
+
+# Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+./emsdk activate latest
+
 Write-Host "Initializing done"
 Read-Host "Press 'enter' to exit"
+
+cd "$PSScriptRoot"
