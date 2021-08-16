c3e303f8c286d769654f1d8af14af12f84b7236e
Harold Groenenboom <harold_groe@hotmail.com>
2021-08-16 22:41:51 +0200

    WIP

--- compile-to-js.ps1
+++ compile-to-js.ps1
@@ -2,4 +2,6 @@
 # This compiler is available once you've installed emscripten
 # See guide: https://emscripten.org/docs/getting_started/downloads.html#installation-instructions-using-the-emsdk-recommended
 # make sure to invoke the `emsdk_env.ps1` from the emsdk first to add `emcc` to your local shells enviroment variables
+. "$PSScriptRoot/submodules/emscripten/emsdk_env.ps1"
+
 emcc $PSScriptRoot/source/main.c -o $PSScriptRoot/build/index
---
+++ initialize.ps1
@@ -0,0 +1,4 @@
+git submodule update --init --recursive
+
+Write-Host "Initializing done"
+Read-Host "Press 'enter' to exit"
